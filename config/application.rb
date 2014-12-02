def72fc2dfc951a0975a0dea98b37ebbac5d7e3a
HiroshiOyamada <h5y1m141@gmail.com>
2014-12-02 14:38:57 +0900

    TwitterアカウントでのOAuth認証完了したらログイン完了したメッセージ含めてリダイレクトするように実装

--- config/application.rb
+++ config/application.rb
@@ -15,16 +15,10 @@ Bundler.require(*Rails.groups)
 
 module AwesomeEvents
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    I18n.available_locales = [:en, :ja]
+    I18n.enforce_available_locales = true
+    I18n.default_locale = :ja
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
   end
 end
