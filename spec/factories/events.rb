4cbc92e15a62ca147c85e9dbe2d67c4489588b7b
HiroshiOyamada <h5y1m141@gmail.com>
2014-12-05 13:18:46 +0900

    FactoryGirlの設定のみ作成

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
 FactoryGirl.define do
   factory :event do
-    
+    owner
+    sequence(:name) { |i| "イベント名#{i}"}
+    sequence(:place) { |i| "場所#{i}"}
+    sequence(:content) { |i| "内容#{i}"}
+    start_time { rand(1..30).days.from_now }
+    end_time {start_time + rand(1..30).hours }
   end
-
 end
