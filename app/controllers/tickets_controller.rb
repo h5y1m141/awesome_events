a28d0a18624107ab070e70fb56d13f09097843ff
HiroshiOyamada <h5y1m141@gmail.com>
2014-12-09 10:08:10 +0900

    イベントキャンセルする機能も実装

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -20,5 +20,11 @@ class TicketsController < ApplicationController
     end
   end
 
+  def destroy
+    ticket = current_user.tickets.find_by!(event_id: params[:event_id])
+    ticket.destroy!
+    redirect_to event_path(params[:event_id], notice: t("controllers.tickets.cancel"))
+  end
+
 
 end
